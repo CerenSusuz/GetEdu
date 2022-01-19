120f50e10dd50788339cf9723f6302fcbd3d2ebe
Ceren Susuz <ceren199704@hotmail.com>
2022-01-19 16:45:57 +0300

    added aspects

---
+++ BaseCore/Aspects/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using BaseCore.Utilities.Interceptors;
+using Castle.Core.Interceptor;
+using Castle.DynamicProxy;
+
+namespace BaseCore.Aspects.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (Exception)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ BaseCore/Utilities/Interceptors/MethodInterceptionBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Castle.Core.Interceptor;
+
+namespace BaseCore.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public abstract class MethodInterceptionBase : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
