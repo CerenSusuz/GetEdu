e09b39f0bda19fa10a2ebb2abcd28c748ae10500
Ceren Susuz <ceren199704@hotmail.com>
2022-01-20 11:19:59 +0300

    added dtos for user methods

---
+++ EntityLayer/Entities/DTOs/BaseDto/UserDto/EditDto.cs
@@ -0,0 +1,18 @@
+﻿using BaseCore.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities.DTOs.BaseDto.UserDto
+{
+    public class EditDto : IDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
